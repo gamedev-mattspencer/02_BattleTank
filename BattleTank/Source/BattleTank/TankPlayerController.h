4a4e965b5761e4d6c1dde4bbfae2d66c5600396d
gamedev_mattspencer <gamedev.mattspencer@gmail.com>
2019-02-24 11:01:10 -0800

    Added tank controller and tank pawn class

---
+++ BattleTank/Source/BattleTank/TankPlayerController.h
@@ -0,0 +1,27 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/PlayerController.h"
+#include "BattleTank/Public/Tank.h"
+#include "TankPlayerController.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class BATTLETANK_API ATankPlayerController : public APlayerController
+{
+	GENERATED_BODY()
+
+public:
+	void BeginPlay() override;
+
+	void Tick(float DeltaTime) override;
+
+	ATank* GetControlledTank() const;
+
+private:
+	
+};
