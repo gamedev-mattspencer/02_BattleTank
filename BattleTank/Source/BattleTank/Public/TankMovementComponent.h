6ceeb3b24fc82fb9b4874a2ddf983b5fb0c1f3d1
gamedev_mattspencer <gamedev.mattspencer@gmail.com>
2019-03-13 19:29:14 -0700

    Cleaned up code

--- BattleTank/Source/BattleTank/Public/TankMovementComponent.h
+++ BattleTank/Source/BattleTank/Public/TankMovementComponent.h
@@ -25,10 +25,12 @@ public:
 	UFUNCTION(BlueprintCallable, Category = Input)
 	void IntendTurnRight(float Throw);
 
-	//TODO check best protection
+private:
+
+	UTankTrack* LeftTrack = nullptr;
+
+	UTankTrack* RightTrack = nullptr;
+
 	virtual void RequestDirectMove(const FVector & MoveVelocity, bool bForceMaxSpeed) override;
 
-private:
-	UTankTrack* LeftTrack = nullptr;
-	UTankTrack* RightTrack = nullptr;
 };
